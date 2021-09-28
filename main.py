5b73870213238b9300a5b18ca8d94be0f4c180b3
Ryan-Sandhoefner <Mngamer813@gmail.com>
2021-09-28 09:26:38 -0500

    One

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-#WRITE YOUR CODE IN THIS FILE
+#define function
+def greaterThan(x,y):
+    if x>y:
+        return True
+
+    else:
+        return False
+
+#define function
+def lessThan(x,y):
+    if x<y:
+        return True
+
+    else:
+        return False
+
+#define function
+def equalTo(x,y):
+    if x==y:
+        return True
+
+    else:
+        return False
+
+#define function
+def greaterOrEqual(x,y):
+    if x>=y:
+        return True
+
+    else:
+        return False
+
+#define function
+def lessOrEqual(x,y):
+    if x<=y:
+        return True
+
+    else:
+        return False
